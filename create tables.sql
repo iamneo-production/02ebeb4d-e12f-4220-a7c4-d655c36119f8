d40ffb526a7cba7cfca40b25b07fed2c7cc9d2de
k vedavathi <123635061+vedavathikarnam@users.noreply.github.com>
2023-06-26 15:10:18 +0530

    Update create tables.sql

--- create tables.sql
+++ create tables.sql
@@ -1,2 +1,3 @@
 create table customer(customer_id char(10) primary key,c_name varchar2(20),
 c_email varchar2(20),contact_number number(10));
+create table customerorder(order_id char(10) primary key,order_date date,customer_id char(10) references customer(customer_id),payment_id char(10));
