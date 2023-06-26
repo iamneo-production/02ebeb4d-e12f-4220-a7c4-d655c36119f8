4fdb6a901bb5f6b6123c1099867a40df1bdd0020
Bharath-Sai-Varma <120109723+Bharath-Sai-Varma@users.noreply.github.com>
2023-06-26 16:11:10 +0530

    seller table added

--- create tables.sql
+++ create tables.sql
@@ -3,4 +3,5 @@ c_email varchar2(20),contact_number number(10));
 create table payment(payment_id char(10) primary key,payment_date date,
 payment_type varchar(20) not null,
 amount int);
+create table seller(seller_id CHAR(20) PRIMARY KEY,s_name VARCHAR2(20) NOT NULL);
 
