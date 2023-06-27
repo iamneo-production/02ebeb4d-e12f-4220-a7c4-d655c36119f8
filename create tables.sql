bde77b909e9d8f7b0ef111e0103bd994d149ec68
tcharansai <89520342+tcharansai@users.noreply.github.com>
2023-06-27 14:45:51 +0530

    Update create tables.sql

--- create tables.sql
+++ create tables.sql
@@ -1,5 +1,6 @@
 create table customer(customer_id char(10) primary key,c_name varchar2(20)not null,
 c_email varchar2(20)not null,contact_number number(10));
+
 create table payment(payment_id char(10) primary key,payment_date date,
 payment_type varchar(20) not null,
 amount int);
