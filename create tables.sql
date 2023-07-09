151f6bfd8d1cf924bc876aba0d58c0788dc619c6
Bharath-Sai-Varma <varmasai785@gmail.com>
2023-07-09 04:48:02 +0000

    table commit

---
+++ create tables.sql
@@ -0,0 +1,33 @@
+create table customer(customer_id char(10) primary key,c_name varchar2(20)not null,
+c_email varchar2(20)not null,contact_number number(10));
+
+create table payment(payment_id char(10) primary key,payment_date date,
+payment_type varchar(20) not null,
+amount int);
+
+create table seller(seller_id CHAR(20) PRIMARY KEY,s_name VARCHAR2(20));
+
+create table  deal(
+  deal_id char(6) primary key,
+  dis_percentage int not null );
+
+create table category(
+  category_id char(5) primary key,
+  c_name varchar(20) not null,
+  sup_category varchar(20) foreign key references category(category_id)
+);
+create table brand(
+  brand_id char(6) primary key,
+  b_name varchar(20) not null);
+
+
+create table shipper(
+  shipper_id char(6) primary key,
+  sh_name varchar(20) not null;
+
+create table location(
+  pincode number(6) primary key,
+  state varchar2(20) not null,
+  city varchar2(20) not null,
+  area_name varchar2(20) not null
+);
