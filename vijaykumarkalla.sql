6679f7073fd22b0f8539af4efca5225ea28f767c
VijayKumarKalla <vijaykalla96@gmail.com>
2023-07-12 12:03:25 +0000

    query1

--- vijaykumarkalla.sql
+++ vijaykumarkalla.sql
@@ -2,7 +2,7 @@ Select * from ECOMMERCE;
 
 SELECT country  FROM ECOMMERCE;
 
-select EXTRACT(Year from DATE) from ECOMMERCE;
+
 -- query to find how many products were sold in february 2019
 Select COUNT(*) as TOTAL FROM
  ECOMMERCE WHERE "Date" >='2019-02-01'
