c78105340e6f85b8d116a0536ea93ca694283e3a
VijayKumarKalla <vijaykalla96@gmail.com>
2023-07-12 11:01:55 +0000

    Query1 sample execution

--- vijaykumarkalla.sql
+++ vijaykumarkalla.sql
@@ -1,1 +1,7 @@
 Select* from ECOMMERCE;
+
+SELECT DATE FROM ECOMMERCE;
+-- query to find how many products were sold in february 2019
+
+Select count(*) as Totalsold From ECOMMERCE where 
+Date>='2019-02-01' And Date<'2019-03-01';
