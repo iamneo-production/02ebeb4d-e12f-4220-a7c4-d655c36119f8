a7cdcb92f9666ad47226cfa7618d5578021d6754
Tejeswar-Kanamarlapudi <tejakanamarlapudi@gmail.com>
2023-07-15 15:20:19 +0000

    shown indexes

--- tejeswar.sql
+++ tejeswar.sql
@@ -4,3 +4,29 @@ desc ECOMMERCE;
 
 select PRODUCTNAME,PRODUCTNO
 FROM ECOMMERCE;
+
+select * 
+from user_INDEXES 
+where TABLE_NAME = 'ecommerce';
+
+DESCRIBE ecommerce;
+
+SELECT *
+FROM user_indexes
+WHERE index_name = 'INDEX_ON_DATE';
+
+SELECT *
+FROM user_indexes
+WHERE index_name = 'INDEX_ON_COUNTRY';
+
+WITH index_data AS (
+    SELECT *
+    FROM user_indexes
+    WHERE index_name = 'INDEX_ON_DATE'
+)
+SELECT *
+FROM index_data;
+
+SELECT *
+FROM user_ind_columns
+WHERE index_name = 'INDEX_ON_DATE';
