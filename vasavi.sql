fc89dc087eadab3dd5e9f29c3fd1b081850b4fd5
Vasavi0000 <vasavi2393@gmail.com>
2023-07-17 05:34:33 +0000

    update

--- vasavi.sql
+++ vasavi.sql
@@ -5,22 +5,27 @@ select * from ecommerce;
 --(1.How many products were sold in february 2019)
 
 select SUM(Quantity) as soldproducts
-from ecommerce where "Date" >='2019-02-01'
-AND "Date" <'2019-03-01';
-
-
---(sql query to total sale amount in each year)
+from ecommerce where "PURCHASE_DATE" >='2019-02-01'
+AND "PURCHASE_DATE" <'2019-03-01';
 
 
 
+--(2.sql query to total sale amount in each year)
+
+select EXTRACT(year from To_Date("PURCHASE_DATE",'YYYY-MM-DD'))
+as Sale_Year,sum(Quantity*Price) as Total_saleamount
+from ECOMMERCE group by "PURCHASE_DATE";
 
 
---(sql query to what was the total sales amount of 
+
+
+--(3.sql query to what was the total sales amount of 
 --each product on a month_wise basis in the year 2019)
 
-
-
-
+SELECT EXTRACT(month from To_Date("PURCHASE_DATE",'YYYY-MM-DD'))as month_sales,
+sum(Quantity*price)as total_sales,productno from Ecommerce WHERE
+extract(year from To_Date("PURCHASE_DATE",'YYYY-MM-DD'))='2019'
+GROUP BY PRODUCTNO,extract(month from To_Date("PURCHASE_DATE",'YYYY-MM-DD'));
 
 
 
@@ -32,7 +37,11 @@ select COUNT (country) from ecommerce group by country;
 
 
 
-
-
 --(5.sql query to list all the unique product names 
---sold from eachb year)
+--sold from each year)
+
+select distinct(PRODUCTNAME),EXTRACT(year from TO_Date
+("PURCHASE_DATE",'YYYY-MM-DD')) 
+as Sale_Year from ECOMMERCE;
+
+
