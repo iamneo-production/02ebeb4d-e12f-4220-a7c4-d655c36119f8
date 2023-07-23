c052a61e99aab7e73598902d6aee6120287a4552
SdRifath <syedrifath1203@gmail.com>
2023-07-23 16:31:08 +0000

    optimized queries

--- rifath_queries.sql
+++ rifath_queries.sql
@@ -2,7 +2,6 @@
 
 /*sql query to how many products were sold in feb 2019*/
 
-set timing on;
 select sum(quantity) from ECOMMERCE
 where extract(year from to_Date(purchase_date, 'yyyy-mm-dd'))
 =2019 and extract(month from to_date(purchase_date, 'yyyy-mm-dd'))=02;
@@ -10,12 +9,12 @@ where extract(year from to_Date(purchase_date, 'yyyy-mm-dd'))
 select sum(quantity) from ecommerce where purchase_date like '2019-02%';
 
 select sum(quantity) from ecommerce where purchase_date between '2019-02-01'
-and '2019-02-29';  /*optimized*/
+and '2019-02-29';     /*optimized*/
 
 select sum(quantity) from ecommerce where substr(purchase_date,1,7)='2019-02';
 
-/*--2--*/
-set timing on;
+/*sql query to find out total sale amount in each year*/
+
 select sum(quantity*price),extract(year from to_date(substr(purchase_date,1,4),'yyyy')) 
 from ecommerce  group by extract(year from to_date(substr(purchase_date,1,4),'yyyy'));
 
@@ -25,7 +24,8 @@ from ecommerce group by extract(year from to_date(purchase_date,'yyyy-mm-dd'));
 select substr(purchase_date,1,4) as sales_year,sum(quantity*price) as totamount
 from ecommerce group by substr(purchase_date,1,4); --optimized--
 
---3--
+/* sql query to find out total sale amount of each product on month basis*/
+
 select extract(month from to_date(PURCHASE_DATE,'yyyy-mm-dd')) as month,productno,sum(price*QUANTITY) from ecommerce 
 where extract(year from to_date(purchase_date,'yyyy-mm-dd'))=2019
 group by extract(month from to_date(PURCHASE_DATE,'yyyy-mm-dd')),PRODUCTNO
@@ -41,29 +41,22 @@ from ECOMMERCE where PURCHASE_DATE like '2019-%'
 group by substr(PURCHASE_DATE,6,2),PRODUCTNO
 order by substr(PURCHASE_DATE,6,2),PRODUCTNO;
 
---4--
-select  country,count(customerno) from ECOMMERCE group by country;
+/* sql query to count the customers from each country*/
 
-SELECT country, COUNT(*) AS customerno
+SELECT country, COUNT(DISTINCT customerno) 
 FROM ecommerce
-GROUP BY country;--opt--
+GROUP BY country;               /*optimized*/
 
-/*SELECT country, COUNT(DISTINCT customerno) AS customer_count
-FROM ecommerce
-GROUP BY country
-ORDER BY customer_count;*/
+/*sql query to list all the unique product names sold from each year*/
 
---5--
 select distinct(extract(year from to_date(purchase_date,'yyyy-mm-dd'))),
 PRODUCTNAME from ECOMMERCE order by
 extract(year from to_date(purchase_date,'yyyy-mm-dd')),productname;
 
 
 select distinct(substr(PURCHASE_DATE,1,4)),productname
-from ecommerce order by substr(PURCHASE_DATE,1,4),PRODUCTNAME;--opt--
+from ecommerce order by substr(PURCHASE_DATE,1,4),PRODUCTNAME;  /*optimized*/
 
-
-select * from ecommerce;
 
 select extract(year from to_date(PURCHASE_DATE,'yyyy-mm-dd')),productname
 from ecommerce
