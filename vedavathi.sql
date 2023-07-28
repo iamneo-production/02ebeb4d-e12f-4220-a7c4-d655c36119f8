5e68865184d6f25b6d3b0e39db129144069b65f0
vedavathikarnam <vedavathikarnam2006@gmail.com>
2023-07-28 13:35:35 +0000

    optimized queries

--- vedavathi.sql
+++ vedavathi.sql
@@ -1,8 +1,7 @@
-set TIMING on;
-desc ECOMMERCE;
+
+
 
 /* 1.  write an sql query to how many products were sold in february 2019 ?*/
-
 
 select sum(quantity) total_products_sold from ECOMMERCE where PURCHASE_DATE >= '2019-02-01' and PURCHASE_DATE <'2019-03-01';
 
@@ -15,12 +14,9 @@ GROUP BY SUBSTR(purchase_date, 1, 4);
 
 /* 3. write a sql query to what was the total sales amount of each product on a month-wise in the year 2019 ?*/
 
-
 select PRODUCTNO,SUBSTR(PURCHASE_DATE,6,2) month, sum(quantity*price) total_sales from ECOMMERCE
 where substr(PURCHASE_DATE,1,4)='2019' 
 group by productno,substr(PURCHASE_DATE,6,2);
-
-    
 
 
 /* 4. write a sql query to count cutomers from each country?*/
