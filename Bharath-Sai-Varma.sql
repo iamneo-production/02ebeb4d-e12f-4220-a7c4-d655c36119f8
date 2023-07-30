23539a98a2aa4b5a807fc59c53943b71e68cced1
Bharath-Sai-Varma <varmasai785@gmail.com>
2023-07-30 16:25:57 +0000

    codes comitted

--- Bharath-Sai-Varma.sql
+++ Bharath-Sai-Varma.sql
@@ -1,42 +1,36 @@
+-- 1.  write an sql query to how many products were sold in february 2019 ?
 
 
---My queries 
+select sum(quantity) as totalproducts_sold from ECOMMERCE where PURCHASE_DATE >= '2019-02-01' and PURCHASE_DATE <'2019-03-01';
 
 
---  1
+--2. write an sql query to total sales amount in each year ? 
+
+SELECT SUBSTR(purchase_date, 1, 4) AS sales_year, SUM(quantity * price) AS totalsales_amount
+FROM ECOMMERCE
+GROUP BY SUBSTR(purchase_date, 1, 4);
 
 
-select sum(quantity) total_products_sold 
-from ECOMMERCE 
-where PURCHASE_DATE >= '2019-02-01' 
-and PURCHASE_DATE <'2019-03-01';
+-- 3. write a sql query to what was the total sales amount of each product on a month-wise in the year 2019 ?
 
--- 2 
-
-select substr(purchase_date, 1, 4) as sales_year , 
-sum(quantity * price) as total_sales_amount
-from ecommerce
-group by substr(purchase_date, 1, 4);            
-
--- 3 
-
-select productno,substr(purchase_date,6,2) month ,
-sum(quantity*price) total_sales from ecommerce 
-where substr(purchase_date,1,4)='2019' 
-group by productno,substr(purchase_date,6,2);                         
+select PRODUCTNO,SUBSTR(PURCHASE_DATE,6,2) as "month", 
+sum(quantity*price) as total_sales from ECOMMERCE
+where substr(PURCHASE_DATE,1,4)='2019' 
+group by productno,substr(PURCHASE_DATE,6,2);
 
 
+--4. write a sql query to count cutomers from each country?
 
--- 4
+SELECT country, COUNT(DISTINCT CUSTOMERNO) AS customer_count
+FROM ECOMMERCE
+GROUP BY country order by country asc;
 
-select country, count(distinct customerno) as customer_count
-from ecommerce
-group by country order by country; 
+-- 5. write an sql query to list all unique product names sold from each year?
 
--- 5
-
-select extract(year from to_date(purchase_date, 'yyyy-mm-dd')) as sales_year, 
-productname as unique_product_names
-from ecommerce
-group by extract(year from to_date(purchase_date, 'yyyy-mm-dd')) , productname 
-order by extract(year from to_date(purchase_date, 'yyyy-mm-dd')) ;
+with CTE AS(
+    SELECT PRODUCTNAME,EXTRACT(YEAR FROM TO_DATE("PURCHASE_DATE",'YYYY-MM-DD')) AS SALES_YEAR 
+FROM ECOMMERCE
+)
+SELECT DISTINCT(PRODUCTNAME) AS UNIQUEPRODUCT_NAME,SALES_YEAR 
+FROM CTE
+GROUP BY SALES_YEAR,PRODUCTNAME ORDER BY SALES_YEAR asc;
