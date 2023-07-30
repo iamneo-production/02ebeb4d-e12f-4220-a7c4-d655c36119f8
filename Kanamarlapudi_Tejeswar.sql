190ae27edcd3d72ea95abd9feb09cda1ad102f74
Tejeswar-Kanamarlapudi <tejakanamarlapudi@gmail.com>
2023-07-30 13:13:09 +0000

    branch commit final

--- Kanamarlapudi_Tejeswar.sql
+++ Kanamarlapudi_Tejeswar.sql
@@ -3,18 +3,21 @@ desc ECOMMERCE;
 set TIMING ON;
 
 -- 1.  write an sql query to how many products were sold in february 2019 ?
-select sum(quantity) from ecommerce where purchase_date between '2019-02-01'
-and '2019-02-29';
+Select sum(QUANTITY) as TotalProductsSold FROM
+ECOMMERCE 
+WHERE PURCHASE_DATE >='2019-02-01'and PURCHASE_DATE<'2019-03-01'; 
 
 -- 2. write an sql query to total sales amount in each year ?
-SELECT SUBSTR(purchase_date, 1, 4) AS sales_year, SUM(quantity * price) AS total_sales_amount
-FROM ECOMMERCE
-GROUP BY SUBSTR(purchase_date, 1, 4);
+SELECT SUBSTR(PURCHASE_DATE,1,4) as "YEAR" ,
+sum(PRICE*QUANTITY) as TotalSalesAmount 
+from ECOMMERCE
+group by SUBSTR(PURCHASE_DATE,1,4); 
 
 -- 3. write a sql query to what was the total sales amount of each product on a month-wise in the year 2019 ?
-select PRODUCTNO,SUBSTR(PURCHASE_DATE,6,2) AS month, sum(quantity*price) AS total_sales 
-from ECOMMERCE where substr(PURCHASE_DATE,1,4)='2019' 
-group by productno,substr(PURCHASE_DATE,6,2);
+Select PRODUCTNO,SUBSTR(PURCHASE_DATE,6,2) as "Month",
+sum(PRICE*QUANTITY) as TOTAL_SALES_AMOUNT from ECOMMERCE
+where SUBSTR(PURCHASE_DATE,1,4)='2019' 
+group by PRODUCTNO,SUBSTR(PURCHASE_DATE,6,2);
 
 -- 4. write a sql query to count cutomers from each country?
 SELECT
@@ -28,8 +31,15 @@ ORDER BY
   country ASC;
 
 -- 5. write an sql query to list all unique product names sold from each year?
-select extract(year from to_date(purchase_date, 'yyyy-mm-dd')) as sales_year, 
-productname as unique_product_names
-from ecommerce
-group by sales_year , productname 
-order by sales_year ASC ;
+WITH CTE AS (
+  	SELECT 
+    PRODUCTNAME, 
+    Extract(YEAR From To_DATE("PURCHASE_DATE", 'YYYY-MM-DD')) AS SALES_YEAR
+  	FROM ECOMMERCE
+)
+SELECT 
+	Distinct(PRODUCTNAME) AS UniqueProductName,
+	SALES_YEAR
+FROM CTE
+GROUP BY SALES_YEAR, PRODUCTNAME
+ORDER BY SALES_YEAR asc;
